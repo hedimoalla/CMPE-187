30a1d172f6d10c499436e82aeeb3b608fd41a3e2
Hedi Moalla <hedimoalla@users.noreply.github.com>
2017-09-26 01:21:10 -0700

    Create Palindrome.java

---
+++ HW4/Palindrome.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(final String[] args) {
+        System.out.print("Input a String: ");
+        final Scanner scanner = new Scanner(System.in);
+        final String inputString = scanner.nextLine();
+        if(inputString != null && !inputString.isEmpty()) {
+            char[] charArray = inputString.toCharArray();
+            if(Palindrome.isPalindrome(charArray)) {
+                System.out.println(inputString + " is a Palindrome!");
+            }
+            else {
+                System.out.println(inputString + " is NOT a Palindrome!");
+            }
+        }
+        else {
+            System.out.println("Invalid input!");
+        }
+    }
+    
+    private static boolean isPalindrome(final char[] charArray) {
+        if(charArray == null || charArray.length == 0) {
+            return false;
+        }
+        else if(charArray.length == 1) {
+            return true;
+        }
+        else if (charArray.length == 2) {
+            return charArray[0] == charArray[1];
+        }
+        else {
+            final int arrayLength = charArray.length;
+            // Compare the first and last element
+            if(charArray[0] != charArray[arrayLength - 1]) {
+                return false;
+            }
+            
+            // Strip the array of the first and last element and continue
+            return isPalindrome(Arrays.copyOfRange(charArray, 1, arrayLength - 1));
+        }
+    }
+}
