e95396300bcd52ed24ce3086d887bf978fd9d8dd
Hedi Moalla <hedimoalla@gmail.com>
2017-10-07 01:19:05 -0700

    Palindrome program

--- HW4/Palindrome.java
+++ HW4/Palindrome.java
@@ -1,54 +1,32 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.LinkedList;
 
-public class Palindrome {
-	
-    public static void main(final String[] args) {
-		String again = "Y";		
-		Scanner input = new Scanner(System.in);
-        
-		do {
-			System.out.print("Input a String: ");
-			final Scanner scanner = new Scanner(System.in);
-			final String inputString = scanner.nextLine();
-			if(inputString != null && !inputString.isEmpty()) {
-				char[] charArray = inputString.toCharArray();
-				if(Palindrome.isPalindrome(charArray)) {
-					System.out.println(inputString + " is a Palindrome!");
-				}
-				else {
-					System.out.println(inputString + " is NOT a Palindrome!");
-				}
-			}
-			else {
-				System.out.println("Invalid input!");
-			}
-			System.out.println("Check another string? Y or N?");
-			again = input.nextLine();
-						
-		} while(!again.equalsIgnoreCase("N"));
-    }
-    
-    private static boolean isPalindrome(final char[] charArray) {
-        if(charArray == null || charArray.length == 0) {
-            return false;
+/* Team 15 */
+
+
+class Palindrome {
+
+    public static void main(String[] args) {
+
+    	System.out.print("Enter any string:");
+        Scanner in=new Scanner(System.in);
+        String inputString = in.nextLine();
+        Queue queue = new LinkedList();
+
+        for (int i = inputString.length()-1; i >=0; i--) {
+            queue.add(inputString.charAt(i));
         }
-        else if(charArray.length == 1) {
-            return true;
+
+        String reverseString = "";
+
+        while (!queue.isEmpty()) {
+            reverseString = reverseString+queue.remove();
         }
-        else if (charArray.length == 2) {
-            return charArray[0] == charArray[1];
-        }
-        else {
-            final int arrayLength = charArray.length;
-            // Compare the first and last element
-            if(charArray[0] != charArray[arrayLength - 1]) {
-                return false;
-            }
-            
-            // Strip the array of the first and last element and continue
-            return isPalindrome(Arrays.copyOfRange(charArray, 1, arrayLength - 1));
-        }
+        if (inputString.equals(reverseString))
+            System.out.println("The input String is a palindrome.");
+        else
+            System.out.println("The input String is not a palindrome.");
+
     }
 }
